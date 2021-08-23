9615f777653faa076b625fd3e371bd462432d7d2
Crowdbotics <team@crowdbotics.com>
2021-08-23 04:53:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "color_match_30033Identifier",
+  url: "https://color_match_30033Identifier.botics.co",
+  api: "https://color_match_30033Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
